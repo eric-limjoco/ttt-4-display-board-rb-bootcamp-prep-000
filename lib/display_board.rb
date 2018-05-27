aa37030c47ff956c4f4b9bc10ef3f54e20542877
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-27 14:47:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  divider = "-----------"
+  
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts divider
+end
